a42f0c854a0198be7c8ec12f2d6e7fe8df56bb06
oktysh <32997006+oktysh@users.noreply.github.com>
2017-12-25 15:13:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("The first step in DEVOPS!");
+    response.end("The first Devops step!");
 
 });
 
